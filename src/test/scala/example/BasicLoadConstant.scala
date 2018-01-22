6708c411812f563b222be496d29c65a2a03d8d8b
koooyooo <koyo.onda@gmail.com>
2018-01-23 00:22:21 +0900

    rename BasicLoad3 -> BasicLoadConstant

--- src/test/scala/example/BasicLoadConstant.scala
+++ src/test/scala/example/BasicLoadConstant.scala
@@ -3,14 +3,15 @@ package example
 import io.gatling.core.Predef._
 import io.gatling.core.scenario.Simulation
 import io.gatling.http.Predef._
+import scala.concurrent.duration._
 
-class BasicLoad3 extends Simulation {
+class BasicLoadConstant extends Simulation {
   val httpConf = http.baseURL("http://computer-database.gatling.io")
 
   val req = http("senario1").get("/").check(status.is(200))
   val scn = scenario("BasicSimulation2").exec(req)
 
   setUp(
-    scn.inject(atOnceUsers(3))
+    scn.inject(constantUsersPerSec(10) during(10 seconds))
   ).protocols(httpConf)
 }
